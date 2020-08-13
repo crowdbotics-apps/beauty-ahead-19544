21043053de2078112bed40decb8a4adeb1dd51b1
Crowdbotics <team@crowdbotics.com>
2020-08-13 20:05:10 +0000

    Update models

--- backend/beauty_ahead_19544/wsgi.py
+++ backend/beauty_ahead_19544/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beauty_ahead_19544.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "beauty_ahead_19544.settings")
 
 application = get_wsgi_application()
